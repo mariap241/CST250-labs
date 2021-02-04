34d4bc67a1185692b543b192c313c5dd914eecd4
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-04 16:39:31 -0600

    testing algorithm

--- lab02b/src/main/java/edu/ru/lab02b/MainClass.java
+++ lab02b/src/main/java/edu/ru/lab02b/MainClass.java
@@ -5,11 +5,11 @@ public class MainClass {
     
     public static void main(String[] args){
         
-        int ar1[]={5, 6, 7, 8, 10, 3, 9};
+        int ar1[]={2, 4, 8, 1};
         
         int ar2[]= mySort(ar1);
         
-        System.out.println("input array: "+ ar1[2]);
+        System.out.println("input array: "+ ar2[0]);
     
      
     }

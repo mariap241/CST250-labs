08ec30e1bf08de33e9789001db5e0838084248f7
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-04 15:45:38 -0600

    added new project

---
+++ lab02b/src/main/java/edu/ru/lab02b/MainClass.java
@@ -0,0 +1,13 @@
+
+package edu.ru.lab02b;
+
+public class MainClass {
+    
+    public static void main(String[] args){
+        
+        int ar1[]={5, 6, 7, 8, 10, 3, 9};
+        System.out.println("input array: "+ ar1[2]);
+    
+    }
+}
+

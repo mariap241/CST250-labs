6168e16486178d99c0ac63f77ac343cdae8e86f6
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-04 16:09:14 -0600

    added sort algorithm

--- lab02b/src/main/java/edu/ru/lab02b/MainClass.java
+++ lab02b/src/main/java/edu/ru/lab02b/MainClass.java
@@ -6,8 +6,32 @@ public class MainClass {
     public static void main(String[] args){
         
         int ar1[]={5, 6, 7, 8, 10, 3, 9};
+        
+        int ar2[]= mySort(ar1);
+        
         System.out.println("input array: "+ ar1[2]);
     
+     
+    }
+    
+    public static int[] mySort(int[] inputArray){
+        
+        int arraySize = inputArray.length;
+        for (int i = 0; i < arraySize - 1; i++){
+            int index=i;
+     
+        for (int j = i + 1; j < arraySize; j++){
+            if (inputArray[j] < inputArray[index]){
+                index = j;
+                
+            }
+        }
+         int temp = inputArray[i];
+         inputArray[i] = inputArray[index];
+         inputArray[index]= temp;
+         
+        } 
+        return inputArray;
     }
 }
 

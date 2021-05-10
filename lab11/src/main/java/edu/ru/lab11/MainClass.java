9c070efc01ad94c94c6853e61eaa94d196720970
Maria Palacios <mariapalacios5889@gmail.com>
2021-05-10 09:32:10 -0500

    adding completed lab 11 code

--- lab11/src/main/java/edu/ru/lab11/MainClass.java
+++ lab11/src/main/java/edu/ru/lab11/MainClass.java
@@ -4,6 +4,13 @@ public class MainClass {
     
    public static void main(String [] args){
        
-       System.out.println("hello world");
+       Node root = new Node(20);
+       root.left = new Node(15);
+       root.right = new Node(30);
+       root.left.left = new Node(10);
+       root.left.right = new Node(12);
+       root.left.right.left = new Node(18);
+       
+       
    } 
 }
--- lab11/src/main/java/edu/ru/lab11/Node.java
+++ lab11/src/main/java/edu/ru/lab11/Node.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ru.lab11;
 
-/**
- *
- * @author mac
- */
 public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+        this.left= null;
+        this.right = null;
+    }
+    
+    
     
 }
